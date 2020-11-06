85d5433597a2c5674364104c0554ea4dbd5a0e8d
Karl Kruus <karlkruus0@gmail.com>
2020-11-06 09:49:16 +0200

    Add electron-json-storage

--- src/main/index.js
+++ src/main/index.js
@@ -25,7 +25,8 @@ function createWindow () {
         useContentSize: true,
         width: 1000,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            enableRemoteModule: true
         }
     });
 
